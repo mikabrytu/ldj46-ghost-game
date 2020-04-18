1d4b42121321f2b87c91276401389e15dacaf99a
Mikabrytu <mikabrytu@gmail.com>
2020-04-18 12:55:53 -0300

    Register all game flow methods on GameManager and add UIManager

--- Ludum Dare 46/Assets/Scripts/Manager/GameManager.cs
+++ Ludum Dare 46/Assets/Scripts/Manager/GameManager.cs
@@ -6,6 +6,116 @@ namespace Mikabrytu.LD46
 {
     public class GameManager : Singleton<GameManager>
     {
+        private UIManager uiManager;
+
+        protected override void Awake()
+        {
+            base.Awake();
+        }
+
+        private void Start()
+        {
+            uiManager = GetComponent<UIManager>();
+        }
+
+        #region Game States
+
+        private void CallMenu()
+        {
+
+        }
+
+        private void CallTutorial()
+        {
+
+        }
+
+        private void CallGame()
+        {
+
+        }
+
+        #endregion
+
+
+        #region Game Lifecycle
+
+        public void StartGame()
+        {
+
+        }
+
+        public void GameOver()
+        {
+
+        }
+
+        #endregion
+
+
+        #region Player
+
+        public void IncreaseFear()
+        {
+
+        }
+
+        public void DecreaseFear()
+        {
+
+        }
+
+        #endregion
+
+
+        #region Spawn
+
+        public void SpawnGhost()
+        {
+
+        }
+
+        public void SpawnBrokenStuff()
+        {
+
+        }
+
+        #endregion
+
+
+        #region Zones
+
+        public void InsideFearZone()
+        {
+
+        }
+
+        public void OutsideFearZone()
+        {
+
+        }
+
+        public void InsideCaptureZone()
+        {
+
+        }
+
+        public void OutsideCaptureZone()
+        {
+
+        }
+
+        public void InsideFixZone()
+        {
+
+        }
+
+        public void OutsideFixZone()
+        {
+
+        }
+
+        #endregion
 
     }
 }
---
+++ Ludum Dare 46/Assets/Scripts/Manager/UIManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Mikabrytu.LD46
+{
+    public class UIManager : Singleton<UIManager>
+    {
+        protected override void Awake()
+        {
+            base.Awake();
+        }
+
+        private void Start()
+        {
+            
+        }
+
+        public void ShowIntroText()
+        {
+
+        }
+    }
+}
