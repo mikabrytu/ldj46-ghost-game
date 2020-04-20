081522dfd411e7f63b3a7b1427daad1e9044c23a
Mikabrytu <mikabrytu@gmail.com>
2020-04-20 00:52:40 -0300

    Add missing components

---
+++ Ludum Dare 46/Assets/Scripts/Component/WarningComponent.cs
@@ -0,0 +1,17 @@
+﻿using Mikabrytu.LD46.Events;
+using UnityEngine;
+
+public class WarningComponent : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider collider)
+    {
+        if (collider.tag == "Player")
+            EventManager.Raise(new BrokenStuffTriggerEvent(true));
+    }
+
+    private void OnTriggerExit(Collider collider)
+    {
+        if (collider.tag == "Player")
+            EventManager.Raise(new BrokenStuffTriggerEvent(false));
+    }
+}
