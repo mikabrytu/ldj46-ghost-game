dc987bb6e7fcff03b16a03cfc33ae8319135f7e6
Mikabrytu <mikabrytu@gmail.com>
2020-04-20 18:24:32 -0300

    Add light data and missing scripts

---
+++ Ludum Dare 46/Assets/Scripts/Manager/AudioManager.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Mikabrytu.LD46
+{
+    public class AudioManager : Singleton<AudioManager>
+    {
+        [SerializeField] private AudioSource footstepSource;
+        [SerializeField] private AudioSource sfxSource;
+        [SerializeField] private AudioSource whisperSource;
+        [SerializeField] private AudioClip soundtrack;
+        [SerializeField] private AudioClip blink, fix, broken, caught;
+
+        private IEnumerator footstepRoutine;
+
+        public void PlayFootstep(bool play)
+        {
+            Debug.Log(play);
+
+            if (play)
+            {
+                if (footstepRoutine != null)
+                    return;
+
+                footstepRoutine = FootstepInterval();
+                StartCoroutine(footstepRoutine);
+            } else
+            {
+                if (footstepRoutine != null)
+                {
+                    StopCoroutine(footstepRoutine);
+                    footstepRoutine = null;
+                }
+            }
+        }
+
+        public void PlayCaught()
+        {
+            if (sfxSource.clip != caught)
+                sfxSource.clip = caught;
+            sfxSource.Play();
+        }
+
+        public void PlayFix()
+        {
+            if (sfxSource.clip != fix)
+                sfxSource.clip = fix;
+            sfxSource.Play();
+        }
+
+        public void PlayBroke()
+        {
+            if (sfxSource.clip != broken)
+                sfxSource.clip = broken;
+            sfxSource.Play();
+        }
+
+        public void PlayWhisper()
+        {
+            whisperSource.Play();
+        }
+
+        public IEnumerator FootstepInterval()
+        {
+            while (true)
+            {
+                footstepSource.Play();
+                yield return new WaitForSeconds(.3f);
+            }
+        }
+    }
+}
